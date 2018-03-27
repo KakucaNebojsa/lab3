df9b9be9fbb1e8d3e9de6fbf8b5b164cfec90709
Stevan Stojicevic <stevanstojicevic95@gmail.com>
2018-03-27 10:31:56 +0200

    lab4

---
+++ drivers/vga_ctrl_v1_00_a/src/vga_ctrl.c
@@ -0,0 +1,14 @@
+/*****************************************************************************
+* Filename:          C:\Users\student\Desktop\RA147\Zadatak4\lab3/drivers/vga_ctrl_v1_00_a/src/vga_ctrl.c
+* Version:           1.00.a
+* Description:       vga_ctrl Driver Source File
+* Date:              Tue Mar 27 09:15:21 2018 (by Create and Import Peripheral Wizard)
+*****************************************************************************/
+
+
+/***************************** Include Files *******************************/
+
+#include "vga_ctrl.h"
+
+/************************** Function Definitions ***************************/
+
---
+++ pcores/vga_ctrl_v1_00_a/hdl/vhdl/text_mem.vhd
@@ -0,0 +1,69 @@
+-------------------------------------------------------------------------------
+--  Department of Computer Engineering and Communications
+--  Author: LPRS2  <lprs2@rt-rk.com>
+--
+--  Module Name: text_mem
+--
+--  Description:
+--
+--    Dual-port RAM for Text (cahar address in char_rom)
+--
+-------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity text_mem is
+  generic(
+    MEM_ADDR_WIDTH : natural := 32;
+    MEM_DATA_WIDTH : natural := 32;
+    MEM_SIZE       : natural := 4800
+    );
+  port(
+	 wr_clk_i  : in std_logic;
+	 rd_clk_i  : in std_logic;
+    clk_i     : in  std_logic;
+    reset_n_i : in  std_logic;             
+    wr_addr_i : in  std_logic_vector(MEM_ADDR_WIDTH-1 downto 0);     -- Slave address input
+    rd_addr_i : in  std_logic_vector(MEM_ADDR_WIDTH-1 downto 0);     -- Slave address input
+    wr_data_i : in  std_logic_vector(MEM_DATA_WIDTH-1 downto 0);     -- Write data output
+    we_i      : in  std_logic;                                       -- 1-write transaction
+    rd_data_o : out std_logic_vector(MEM_DATA_WIDTH-1 downto 0)      -- read data output
+    );
+end entity;
+
+architecture arc_text_mem of text_mem is
+
+  type t_text_mem  is array (0 to MEM_SIZE-1) of  std_logic_vector(MEM_DATA_WIDTH-1 downto 0);
+
+  signal index_t : natural;
+  signal index   : natural;
+
+  signal text_mem : t_text_mem := (
+  --        0 => "000000",
+  --        1 => "000001",
+  --        2 => "000010",
+          others => (others => '0')
+          );
+  
+begin
+  
+DP_WR_GRAPHICS_MEM : process (wr_clk_i) begin
+	if (rising_edge(wr_clk_i)) then
+		if (we_i = '1') then
+			graphics_mem(wr_addr_i) <= wr_data_i;
+		end if;
+	end if;
+end process;
+
+DP_RD_GRAPHICS_MEM : process (rd_clk_i) begin
+	if (rising_edge(rd_clk_i)) then
+	rd_addr <= mem_up_addr;
+	end if;
+end process;
+
+rd_value <= graphics_mem(conv_integer(rd_addr));
+rd_data_o <= rd_value(conv_integer(mem_lo_addr));
+
+end arc_text_mem;
